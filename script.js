490e944f605b9782980d6e833df449a540a1a7b1
maxime belle <45199116+hippopovache@users.noreply.github.com>
2021-03-24 12:08:01 +0100

    ajouts js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
-    const quantite = $("")
-    const prixUnitaire =
+    const quantite = $("#quantite");
+    const prixUnitaire = $("#prixunitaire");
+    let ht = $("#ht");
+
+    $(document).on("keyup", "input", function(){
+        if(quantite.val() && prixUnitaire.val()){
+            ht.val(quantite.val() * prixUnitaire.val())
+        }
+    });
 });
